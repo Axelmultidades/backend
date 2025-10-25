96b66e34f2b6384a5c2dca1b318c728825c5592e
Axelmultidades <aaxel532@gmail.com>
2025-10-25 19:04:37 -0400

    correciones

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://controladorasistencias-production-96e7.up.railway.app'],
+    'allowed_origins' => ['https://controladorasistencias-production-96e7.up.railway.app'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
